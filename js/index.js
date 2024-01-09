7a6bd1eae60b71f729177c18ca32c7d71fa18ec9
Jyothi-Ramesh08 <montage.on.a.monday@gmail.com>
2024-01-08 18:24:34 -0800

    Adding transitions

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in')
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
@@ -12,3 +14,27 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: '0px 0px -250px 0px'
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
+    }
+  })
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});

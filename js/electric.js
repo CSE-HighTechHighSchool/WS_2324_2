017d518875174145ef26e4bba94307fde99c2316
Jyothi-Ramesh08 <montage.on.a.monday@gmail.com>
2024-01-08 17:55:48 -0800

    Adding Javascript Transitions

--- js/electric.js
+++ js/electric.js
@@ -1,6 +1,8 @@
 const tabs = document.querySelector(".elWrapper");
 const tabButton = document.querySelectorAll(".tab-button");
 const contents = document.querySelectorAll(".elContent");
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in')
 
 tabs.onclick = e => {
   const id = e.target.dataset.id;
@@ -17,3 +19,27 @@ tabs.onclick = e => {
     element.classList.add("active");
   }
 }
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: '0px 0px -250px 0px'
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
+    }
+  })
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});
